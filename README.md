fe96074bc2918b86256be7695709f3172814d811
Teyir <60007549+Teyir@users.noreply.github.com>
2021-10-14 11:49:05 +0200

    [IMPROVE] total rewrite (V2.0)

--- README.md
+++ README.md
@@ -1,18 +1,15 @@
 # PAPI Random Expansion
 
 ## Expansions
-This is all the placeholders (V1.0.1)
+This is all the placeholders (V2.0.0)
 
-| Placeholder | RANGE |
-| ------ | ------ |
-| %random_10% | 0 - 10  |
-| %random_32% | 0 - 32  |
-| %random_64% | 0 - 64  |
-| %random_100% | 0 - 100  |
-| %random_128% | 0 - 128  |
-| %random_256% | 0 - 256  |
-| %random_1000% | 0 - 1000  |
-| %random_2304% | 0 - 2304  |
+| Placeholder | Return |
+| ------ |------ |
+| %random_max_NUMBER% | Int  |
+
+#Example
+%random_max_150% -> 80
+
 
 
 
--- src/main/java/fr/teyir/random/Random.java
+++ src/main/java/fr/teyir/random/Random.java
@@ -1,6 +1,5 @@
 package fr.teyir.random;
 
-
 import org.bukkit.entity.Player;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
@@ -29,7 +28,7 @@ public class Random extends PlaceholderExpansion {
     // Version de l'expansion
     @Override
     public String getVersion() {
-        return "1.0.1";
+        return "2.0.0";
     }
 
 
@@ -56,25 +55,13 @@ public class Random extends PlaceholderExpansion {
 
     public String onPlaceholderRequest(Player p, String identifier) {
 
+        //Max random number
+        if(identifier.toLowerCase().startsWith("max_")){
+            String value=identifier.split("(?i)max_")[1];
 
+            int maxRandom = Integer.parseInt(value);
 
-        switch(identifier) {
-            case "10":
-                return String.valueOf(new java.util.Random().nextInt(10));
-            case "32":
-                return String.valueOf(new java.util.Random().nextInt(32));
-            case "64":
-                return String.valueOf(new java.util.Random().nextInt(64));
-            case "100":
-                return String.valueOf(new java.util.Random().nextInt(100));
-            case "128":
-                return String.valueOf(new java.util.Random().nextInt(128));
-            case "256":
-                return String.valueOf(new java.util.Random().nextInt(256));
-            case "1000":
-                return String.valueOf(new java.util.Random().nextInt(1000));
-            case "2304":
-                return String.valueOf(new java.util.Random().nextInt(2304));
+            return String.valueOf(new java.util.Random().nextInt(maxRandom));
 
         }
 
@@ -84,8 +71,6 @@ public class Random extends PlaceholderExpansion {
             return "Le plus beau bien évidement <3<3";
         }
 
-
-
-        return "&cNombre invalide, voici la liste: 10 - 32 - 64 - 100 - 128 - 256 - 1000 - 2304";
+        return "&cPlaceholder invalide";
     }
 }
